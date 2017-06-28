9341ee0652cf65093da85377e38c9b10f07e19c9
PrabhuIT <prabhuit.23@gmail.com>
2017-06-28 15:04:30 +0530

    Vowel
    C Program

---
+++ vowel.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+char c;
+printf("Enter the string:");
+scanf("%c",&c)
+if(c="a"||c="e"||c="i"||c="o"||c="u")
+printf("it is vowel");
+else
+printf("Its a constant");
+}
