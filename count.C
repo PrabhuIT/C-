055bd69a7c7ac971477634722f3b3dd7978a8649
PrabhuIT <prabhuit.23@gmail.com>
2017-06-30 13:55:47 +0530

    COUNT
    INTEGERS

---
+++ count.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+long long a;
+int b=0;
+printf("enter the numbers");
+scanf("%d",&a);
+b=log(a)+1;
+printf("the total values are %d",b);
+return 0;
+}
