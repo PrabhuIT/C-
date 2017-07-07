2c7a887e1736accc999142bf93f331d5de9c6b6a
PrabhuIT <prabhuit.23@gmail.com>
2017-07-07 12:05:03 +0530

    PALINDROME
    NUMBER

---
+++ Palindrome.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,a=0,b;
+printf("enter the number");
+scanf("%d",&n);
+b=n;
+while(c!=0)
+{
+b=b*10;
+b=b+c%10;
+b=b/10;
+}
+if(n==a)
+printf("%d is palindrome \n",n);
+else
+printf("%d is not palindrome \n",n);
+return 0;
+}
