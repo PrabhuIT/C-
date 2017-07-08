8be9cdb72900e9f6040bf0303bbf10c5ec6ae915
PrabhuIT <prabhuit.23@gmail.com>
2017-07-08 11:50:42 +0530

    POWER
    NUMBER

---
+++ power.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+double a,b,c;
+printf("enter the a and b values");
+scanf("%lf%lf",&a,&b);
+pow(a,b);
+printf("%.2lf is to %lf.2f=%.2lf\n",a,b,c);
+return o;
+}
